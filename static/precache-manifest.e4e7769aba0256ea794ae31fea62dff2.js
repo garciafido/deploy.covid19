a7127810e87e8b2a52d38c7a9b5a6eb1915ba08e
Fido García <garciafido@gmail.com>
2020-07-24 10:31:54 -0300

    Covid19

---
+++ static/precache-manifest.e4e7769aba0256ea794ae31fea62dff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe736cb5665cbcb734e858c08fe2a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f7869eaf2031f03d59",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c43897d39e8436191930",
+    "url": "/static/js/2.94cb18f1.chunk.js"
+  },
+  {
+    "revision": "b81f235e8c08e8f83f269c6bd9dc2bc5",
+    "url": "/static/js/2.94cb18f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f7869eaf2031f03d59",
+    "url": "/static/js/main.f6961f13.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  }
+]);
