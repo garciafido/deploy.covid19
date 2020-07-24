ad5f369a474f49a4b57bebab4abb37fc427b920c
Fido García <garciafido@gmail.com>
2020-07-24 17:37:46 -0300

    fix tucuman

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccb27b9647eb3aa75de032b3576cafdd.js"
+  "/precache-manifest.486421bb0555e3a608ae6711d78e28e3.js"
 );
 
 self.addEventListener('message', (event) => {
