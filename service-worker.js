e61e0285af567c6bfdab33f71c85c4639b70cb7b
Fido García <garciafido@gmail.com>
2020-08-21 09:29:59 -0300

    new sample data

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5aca98fdde24fee0ece52f882d79f894.js"
+  "/precache-manifest.23d93260434ddbe790efdbdc3dacc1bb.js"
 );
 
 self.addEventListener('message', (event) => {
